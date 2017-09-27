7702bdfe0876878b52c27e7f1d4aecc044b1e8ac
Rebecca Kern <rkern@andrew.cmu.edu>
2017-09-27 11:20:09 -0400

    started setting up authors controller

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,2 +1,18 @@
 class AuthorsController < ApplicationController
+  before_action :set_author, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @authors = Author.active.alphabetical.paginate(:page => params[:page]).per_page(10)
+  end
+
+  private
+
+  def author_params
+    params.require(:author).permit(:first_name, :last_name, :active)
+  end
+
+  def set_author
+    @author = Author.find(params[:id])
+  end
+
 end
